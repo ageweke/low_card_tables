df9867eaebe777b9f76024a579ce492906336755
Andrew Geweke <andrew@errorsea.com>
2013-10-16 17:08:22 -0700

    Refactor BasicSystemSpec a little.

--- spec/low_card_tables/system/basic_system_spec.rb
+++ spec/low_card_tables/system/basic_system_spec.rb
@@ -4,12 +4,28 @@ require 'active_record/migration'
 require 'low_card_tables/helpers/database_helper'
 
 describe LowCardTables do
+  def migrate(&block)
+    migration_class = Class.new(::ActiveRecord::Migration)
+    metaclass = migration_class.class_eval { class << self; self; end }
+    metaclass.instance_eval { define_method(:up, &block) }
+    migration_class.migrate(:up)
+  end
+
+  def define_model_class(name, table_name, &block)
+    model_class = Class.new(::ActiveRecord::Base)
+    ::Object.const_set(name, model_class)
+    model_class.table_name = table_name
+    model_class.class_eval(&block)
+  end
+
   before :each do
     @dh = LowCardTables::Helpers::DatabaseHelper.new
     @dh.setup_activerecord!
+  end
 
-    class ::CreateUserStatuses < ::ActiveRecord::Migration
-      def self.up
+  context "with normal table setup" do
+    before :each do
+      migrate do
         drop_table :lctables_spec_user_statuses rescue nil
         create_table :lctables_spec_user_statuses do |t|
           t.boolean :deleted, :null => false
@@ -19,15 +35,7 @@ describe LowCardTables do
         end
 
         add_index :lctables_spec_user_statuses, [ :deleted, :deceased, :gender, :donation_level ], :unique => true, :name => 'index_lctables_spec_user_statuses_on_all'
-      end
 
-      def self.down
-        drop_table :lctables_spec_user_statuses
-      end
-    end
-
-    class ::CreateUsers < ::ActiveRecord::Migration
-      def self.up
         drop_table :lctables_spec_users rescue nil
         create_table :lctables_spec_users do |t|
           t.string :name, :null => false
@@ -35,30 +43,32 @@ describe LowCardTables do
         end
       end
 
-      def self.down
-        drop_table :lctables_spec_users
+      define_model_class(:UserStatus, 'lctables_spec_user_statuses') { is_low_card_table }
+      define_model_class(:User, 'lctables_spec_users') { has_low_card_table :status }
+    end
+
+    after :each do
+      migrate do
+        drop_table :lctables_spec_user_statuses rescue nil
+        drop_table :lctables_spec_users rescue nil
       end
     end
 
-    # ::ActiveRecord::Migration.run(CreateUserStatuses)
-    ::CreateUserStatuses.migrate(:up)
-    ::CreateUsers.migrate(:up)
+    it "should say #is_low_card_table? appropriately" do
+      ::UserStatus.is_low_card_table?.should be
+      ::User.is_low_card_table?.should_not be
 
-    class ::UserStatus < ActiveRecord::Base
-      self.table_name = "lctables_spec_user_statuses"
-
-      is_low_card_table
+      ::UserStatus.low_card_options.should == { }
     end
 
-    class ::User < ActiveRecord::Base
-      self.table_name = "lctables_spec_users"
-
-      has_low_card_table :status
+    it "should allow setting all options, and create an appropriate row" do
+      u = ::User.new
+      u.name = 'User1'
+      u.deleted = false
+      u.deceased = false
+      u.gender = 'female'
+      u.donation_level = 3
+      u.save!
     end
-  end
-
-  it "should say #is_low_card_table? appropriately" do
-    ::UserStatus.is_low_card_table?.should be
-    ::User.is_low_card_table?.should_not be
   end
 end
