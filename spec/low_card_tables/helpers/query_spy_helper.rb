01777cbafcdf419134b0f67dec7c0bec603e7132
Andrew Geweke <andrew@errorsea.com>
2013-10-27 12:42:02 -0700

    Factor out a QuerySpyHelper, and check that we don't re-query the table more than twice when adding a row or rows.

---
+++ spec/low_card_tables/helpers/query_spy_helper.rb
@@ -0,0 +1,47 @@
+module LowCardTables
+  module Helpers
+    class QuerySpyHelper
+      class << self
+        def with_query_spy(*args, &block)
+          new(*args).spy(&block)
+        end
+      end
+
+      def initialize(table_name)
+        @table_name = table_name
+        @calls = [ ]
+      end
+
+      def spy(&block)
+        begin
+          register!
+          block.call(self)
+        ensure
+          deregister!
+        end
+      end
+
+      def call_count
+        @calls.length
+      end
+
+      def call(notification_name, when1, when2, id, data)
+        sql = data[:sql]
+        if sql && sql.strip.length > 0
+          if sql =~ /^\s*SELECT.*FROM\s+['"]*\s*#{@table_name}\s*['"]*\s+/mi
+            @calls << data.merge(:backtrace => caller)
+          end
+        end
+      end
+
+      private
+      def register!
+        ActiveSupport::Notifications.subscribe("sql.active_record", self)
+      end
+
+      def deregister!
+        ActiveSupport::Notifications.unsubscribe(self)
+      end
+    end
+  end
+end
--- spec/low_card_tables/system/caching_system_spec.rb
+++ spec/low_card_tables/system/caching_system_spec.rb
@@ -1,6 +1,7 @@
 require 'low_card_tables'
 require 'low_card_tables/helpers/database_helper'
 require 'low_card_tables/helpers/system_helpers'
+require 'low_card_tables/helpers/query_spy_helper'
 
 describe LowCardTables do
   include LowCardTables::Helpers::SystemHelpers
@@ -13,50 +14,65 @@ describe LowCardTables do
     create_standard_system_spec_models!
 
     ::UserStatus.low_card_cache_expiration = :unlimited
-
-    @_ar_sql_calls = [ ]
-
-    @_ar_sql_spy = lambda do |notification_name, when1, when2, id, data|
-      sql = data[:sql]
-      if sql && sql.strip.length > 0
-        if sql =~ /^\s*SELECT.*FROM\s+['"]*\s*lctables_spec_user_statuses\s*['"]*\s+/mi
-          @_ar_sql_calls << true
-          $stderr.puts ">>> MATCH: #{sql}\n#{caller.join("\n")}"
-        end
-      end
-    end
-
-    ActiveSupport::Notifications.subscribe("sql.active_record", @_ar_sql_spy)
   end
 
   after :each do
     drop_standard_system_spec_tables!
-    ActiveSupport::Notifications.unsubscribe(@_ar_sql_spy)
   end
 
   it "should cache low-card rows in memory" do
-    @_ar_sql_calls.length.should == 0
+    LowCardTables::Helpers::QuerySpyHelper.with_query_spy("lctables_spec_user_statuses") do |spy|
+      spy.call_count.should == 0
 
-    user1 = ::User.new
-    user1.name = 'User1'
-    user1.deleted = false
-    user1.deceased = false
-    user1.gender = 'female'
-    user1.donation_level = 3
-    user1.save!
+      user1 = ::User.new
+      user1.name = 'User1'
+      user1.deleted = false
+      user1.deceased = false
+      user1.gender = 'female'
+      user1.donation_level = 3
+      user1.save!
 
-    mid_calls = @_ar_sql_calls.length
-    @_ar_sql_calls.length.should > 0
+      mid_calls = spy.call_count
+      spy.call_count.should > 0
 
-    user2 = ::User.new
-    user2.name = 'User2'
-    user2.deleted = false
-    user2.deceased = false
-    user2.gender = 'female'
-    user2.donation_level = 3
-    user2.save!
+      user2 = ::User.new
+      user2.name = 'User2'
+      user2.deleted = false
+      user2.deceased = false
+      user2.gender = 'female'
+      user2.donation_level = 3
+      user2.save!
 
-    @_ar_sql_calls.length.should == mid_calls
+      spy.call_count.should == mid_calls
+    end
+  end
+
+  it "should purge its cache efficiently when adding a new row" do
+    LowCardTables::Helpers::QuerySpyHelper.with_query_spy("lctables_spec_user_statuses") do |spy|
+      spy.call_count.should == 0
+
+      user1 = ::User.new
+      user1.name = 'User1'
+      user1.deleted = false
+      user1.deceased = false
+      user1.gender = 'female'
+      user1.donation_level = 3
+      user1.save!
+
+      mid_calls = spy.call_count
+      spy.call_count.should > 0
+
+      user2 = ::User.new
+      user2.name = 'User2'
+      user2.deleted = false
+      user2.deceased = false
+      user2.gender = 'male'
+      user2.donation_level = 7
+      user2.save!
+
+      # We allow two calls here because we need it for our double-checked locking pattern.
+      (spy.call_count - mid_calls).should <= 2
+    end
   end
 
   it "should notify listeners when refreshing its cache"
