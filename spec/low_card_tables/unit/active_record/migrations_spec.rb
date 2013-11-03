49ed1312cf442ba2e1ea3aafa54634a5279d4fb3
Andrew Geweke <andrew@errorsea.com>
2013-11-02 18:47:41 -0700

    Spec out much of the migration-checking flow.

--- spec/low_card_tables/unit/active_record/migrations_spec.rb
+++ spec/low_card_tables/unit/active_record/migrations_spec.rb
@@ -25,13 +25,49 @@ describe LowCardTables::ActiveRecord::Migrations do
 
   before :each do
     @migration = MockMigrationClass.new
+    @opts = Hash.new
+    @proc = lambda { }
   end
 
   it "should pass through correctly for :create_table" do
-    opts = Hash.new
-    proc = lambda { }
+    @migration.create_table(:foo, @opts, &@proc)
+    @migration.calls.should == [ { :name => :create_table, :args => [ :foo, @opts ], :block => @proc } ]
+  end
 
-    @migration.create_table(:foo, opts, &proc)
-    @migration.calls.should == [ { :name => :create_table, :args => [ :foo, { } ], :block => proc } ]
+  context "with mock ::Rails" do
+    before :each do
+      rails_class = Object.new
+      Object.const_set(:Rails, rails_class)
+
+      application = Object.new
+
+      expect(rails_class).to receive(:application).once.and_return(application)
+      expect(application).to receive(:eager_load!).once
+    end
+
+    it "should call #eager_load, pick up an AR descendant properly, and enforce the index" do
+      class1 = Object.new
+      class2 = Object.new
+
+      expect(class1).to receive(:table_name).and_return('bar')
+      expect(class2).to receive(:table_name).and_return('foo')
+
+      expect(class2).to receive(:is_low_card_table?).and_return(true)
+      expect(class2).to receive(:name).at_least(:once).and_return('Whatever')
+
+      expect(::ActiveRecord::Base).to receive(:descendants).and_return([ class1, class2 ])
+
+      expect(class2).to receive(:_low_card_remove_unique_index!).once.ordered
+
+      expect(class2).to receive(:reset_column_information).exactly(3).times.ordered
+      expect(class2).to receive(:_low_card_value_column_names).twice.ordered.and_return([ 'x', 'y' ])
+
+      expect(LowCardTables::VersionSupport).to receive(:clear_schema_cache!).once.ordered.with(class2)
+
+      expect(class2).to receive(:_low_card_ensure_has_unique_index!).once.with(true).ordered
+
+      @migration.create_table(:foo, @opts, &@proc)
+      @migration.calls.should == [ { :name => :create_table, :args => [ :foo, @opts ], :block => @proc } ]
+    end
   end
 end
