bc9ac3d7f40e3ee1be4b31bdcd5d4b84867542de
Andrew Geweke <andrew@errorsea.com>
2013-11-02 17:30:27 -0700

    Move _low_card_disable_save_when_needed! to LowCardTables::LowCardTable::Base.

--- lib/low_card_tables/active_record/base.rb
+++ lib/low_card_tables/active_record/base.rb
@@ -13,6 +13,14 @@ module LowCardTables
       include LowCardTables::LowCardTable::CacheExpiration::HasCacheExpiration
 
       module ClassMethods
+        # Declares that this is a low-card table. This should only ever be used on tables that are,
+        # in fact, low-card tables.
+        #
+        # options can contain:
+        #
+        # [:exclude_column_names] Excludes the specified Array of column names from being treated
+        #                         as low-card columns; this happens by default to created_at and
+        #                         updated_at.
         def is_low_card_table(options = { })
           include LowCardTables::LowCardTable::Base
           self.low_card_options = options
@@ -34,40 +42,6 @@ module LowCardTables
 
         def has_any_low_card_tables?
           false
-        end
-
-        def _low_card_disable_save_when_needed!
-          send(:define_method, :save_low_card_row!) do |*args|
-            begin
-              @_low_card_saves_allowed = true
-              save!(*args)
-            ensure
-              @_low_card_saves_allowed = false
-            end
-          end
-
-          %w{save save!}.each do |method_name|
-            send(:define_method, method_name) do |*args|
-              if @_low_card_saves_allowed
-                super(*args)
-              else
-                raise LowCardTables::Errors::LowCardCannotSaveAssociatedLowCardObjectsError, %{You just tried to save a model that represents a row in a low-card table.
-You can't do this, because the entire low-card system relies on the fact that low-card rows
-are immutable once created. Changing this row would therefore change the logical state of
-many, many rows that are associated with this one, and that is almost certainly not what
-you want.
-
-Instead, simply modify the low-card attributes directly -- typically on the associated object
-(e.g., my_user.deleted = true), or on the low-card object (my_user.status.deleted = true),
-and then save the associated object instead (my_user.save!). This will trigger the low-card
-system to recompute which low-card row the object should be associated with, and update it
-as needed, which is almost certainly what you actually want.
-
-If you are absolutely certain you know what you're doing, you can call #save_low_card_row!
-on this object, and it will save, but make sure you understand ALL the implications first.}
-              end
-            end
-          end
         end
       end
     end
