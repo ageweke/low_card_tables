8434af719413a9a314773d68a1fe919d1157ff49
Andrew Geweke <andrew@errorsea.com>
2013-11-03 11:06:26 -0800

    Documentation for LowCardTables::ActiveRecord::Migrations.

--- lib/low_card_tables/active_record/migrations.rb
+++ lib/low_card_tables/active_record/migrations.rb
@@ -5,21 +5,36 @@ require 'low_card_tables/has_low_card_table/base'
 
 module LowCardTables
   module ActiveRecord
+    # This module gets included into ::ActiveRecord::Migrations, and overrides key methods (using alias_method_chain)
+    # to add low-card support. Its job is to detect if a low-card table is being modified, and, if so:
+    #
+    # * Remove the all-columns unique index before the operation in question, and add it back afterwards
+    # * If a column has been removed, collapse any now-duplicate rows in question and update all referring tables
+    #
+    # It also adds a single method to migrations, #change_low_card_table, which does nothing of its own rather than
+    # to call the passed block -- but it does all the checking above at the start and end, and disables any such
+    # checking within the block. It thus gives you control over exactly when this happens.
     module Migrations
       extend ActiveSupport::Concern
 
+      # Overrides ::ActiveRecord::Migrations#create_table with low-cardinality support, as described in the comment
+      # for LowCardTables::ActiveRecord::Migrations.
       def create_table_with_low_card_support(table_name, options = { }, &block)
         ::LowCardTables::ActiveRecord::Migrations.with_low_card_support(table_name, options) do |new_options|
           create_table_without_low_card_support(table_name, new_options, &block)
         end
       end
 
+      # Overrides ::ActiveRecord::Migrations#add_column with low-cardinality support, as described in the comment
+      # for LowCardTables::ActiveRecord::Migrations.
       def add_column_with_low_card_support(table_name, column_name, type, options = {})
         ::LowCardTables::ActiveRecord::Migrations.with_low_card_support(table_name, options) do |new_options|
           add_column_without_low_card_support(table_name, column_name, type, options)
         end
       end
 
+      # Overrides ::ActiveRecord::Migrations#remove_column with low-cardinality support, as described in the comment
+      # for LowCardTables::ActiveRecord::Migrations.
       def remove_column_with_low_card_support(table_name, *column_names)
         options = column_names.pop if column_names[-1] && column_names[-1].kind_of?(Hash)
         ::LowCardTables::ActiveRecord::Migrations.with_low_card_support(table_name, options) do |new_options|
@@ -30,6 +45,8 @@ module LowCardTables
         end
       end
 
+      # Overrides ::ActiveRecord::Migrations#change_table with low-cardinality support, as described in the comment
+      # for LowCardTables::ActiveRecord::Migrations.
       def change_table_with_low_card_support(table_name, options = { }, &block)
         ::LowCardTables::ActiveRecord::Migrations.with_low_card_support(table_name, options) do |new_options|
           ar = method(:change_table_without_low_card_support).arity
@@ -41,6 +58,16 @@ module LowCardTables
         end
       end
 
+      # Given the name of a low-card table and a block:
+      #
+      # * Removes the all-columns unique index for that low-card table;
+      # * Calls the block;
+      # * Looks for any removed columns, and, if so, collapses now-duplicate rows and updates all referrers;
+      # * Creates the all-columns unique index for that table.
+      #
+      # While inside the block, none of the above checking will be performed against that table, as it otherwise would
+      # be if you call #add_column, #remove_column, #create_table, or #change_table. This thus gives you a scope in
+      # which to do what you need to do, without the all-columns index interfering.
       def change_low_card_table(table_name, &block)
         ::LowCardTables::ActiveRecord::Migrations.with_low_card_support(table_name, { :low_card => true }) do |new_options|
           block.call
@@ -55,7 +82,17 @@ module LowCardTables
       end
 
       class << self
+        # Adds all the checking described in the comment on LowCardTables::ActiveRecord::Migrations for the given
+        # table name to the supplied block.
         def with_low_card_support(table_name, options = { }, &block)
+          # Don't do this if we're already inside such a check -- this is needed because:
+          #
+          #    change_table :foo do |t|
+          #      t.remove :bar
+          #    end
+          #
+          # ...actually translates internally to a call to remove_column inside change_table, and, otherwise, we'll
+          # try to do our work twice, which is bad news.
           return block.call(options) if inside_migrations_check?
 
           (options, low_card_options) = partition_low_card_options(options)
@@ -73,10 +110,12 @@ module LowCardTables
         end
 
         private
+        # Are we currently inside a call to #with_low_card_support?
         def inside_migrations_check?
           !! Thread.current[:_low_card_migrations_only_once]
         end
 
+        # Wrap the given block in code notifying us that we're inside a call to #with_low_card_support.
         def with_migrations_check(&block)
           begin
             Thread.current[:_low_card_migrations_only_once] = true
@@ -86,6 +125,8 @@ module LowCardTables
           end
         end
 
+        # Wrap the given block in code that checks to see if we've removed any columns from the table that the given
+        # model is for, and, if so, calls low_card_collapse_rows_and_update_referrers! on that model.
         def with_removed_column_detection(model, low_card_options, &block)
           previous_columns = fresh_value_column_names(model)
 
@@ -102,20 +143,27 @@ module LowCardTables
           end
         end
 
+        # Wrap the given block in code that removes the all-columns unique index on the given model (which must be
+        # a low-card table) beforehand, and recreates it afterwards.
         def without_unique_index(model, low_card_options, &block)
           begin
             model._low_card_remove_unique_index!
             block.call
           ensure
-            model._low_card_ensure_has_unique_index!(true) unless low_card_options.has_key?(:low_card_collapse_rows) && (! low_card_options[:low_card_collapse_rows])
+            unless low_card_options.has_key?(:low_card_collapse_rows) && (! low_card_options[:low_card_collapse_rows])
+              model._low_card_ensure_has_unique_index!(true)
+            end
           end
         end
 
+        # Gets a fresh set of _low_card_value_column_names from the given low-card model.
         def fresh_value_column_names(model)
           model.reset_column_information
           model._low_card_value_column_names
         end
 
+        # Splits an options Hash into two -- the first containing everything but low-card-related options, the second
+        # containing only low-card options.
         def partition_low_card_options(options)
           options = (options || { }).dup
           low_card_options = { }
@@ -129,12 +177,22 @@ module LowCardTables
           [ options, low_card_options ]
         end
 
+        # Given a table name, looks to see if it's a low-card table -- either implicitly, because there's an existing
+        # model for that table that declares itself to be a low-card model, or explicitly, because we were passed
+        # :low_card => true in the options hash. If so, returns a model to use for that table -- either the existing
+        # model (implicit case) or a newly-created temporary model class (explicit case).
+        #
+        # Prefers an existing model over a temporary model, if there's both an existing model and we were passed
+        # :low_card => true.
         def low_card_model_to_use_for(table_name, low_card_options)
           out = existing_low_card_model_for(table_name)
           out ||= temporary_model_class_for(table_name) if low_card_options[:low_card]
           out
         end
 
+        # Creates a temporary low-card model class for the given table_name. This is used only if we explicitly
+        # declare a model to be low-card in a migration, but there isn't currently a model for that table that
+        # declares itself to be low-card.
         def temporary_model_class_for(table_name)
           temporary_model_class = Class.new(::ActiveRecord::Base)
           temporary_model_class.table_name = table_name
@@ -143,9 +201,15 @@ module LowCardTables
           temporary_model_class
         end
 
+        # Looks at ::ActiveRecord::Base.descendants to see if there's an existing model class that references the given
+        # table_name and declares itself to be a low-card model class. If so, returns it.
+        #
+        # This method will attempt to eager-load all Rails code, so that we can detect the model class properly.
+        # (Otherwise, migrations typically don't end up loading most models, so, even if the model is there on disk,
+        # it will not be in memory and thus won't appear in ::ActiveRecord::Base.descendants.)
         def existing_low_card_model_for(table_name)
           # Make sure we load all models
-          ::Rails.application.eager_load! if defined?(::Rails)
+          ::Rails.application.eager_load! if defined?(::Rails) && ::Rails.respond_to?(:application) && ::Rails.application && ::Rails.application.respond_to?(:eager_load!)
           out = ::ActiveRecord::Base.descendants.detect do |klass|
             klass.table_name.strip.downcase == table_name.to_s.strip.downcase &&
               klass.is_low_card_table? &&
