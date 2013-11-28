b45458865674a46cac678fb48cbb571b598ce03c
Andrew Geweke <andrew@errorsea.com>
2013-11-27 17:44:36 -0800

    Specs and documentation for Cache.

--- lib/low_card_tables/low_card_table/cache.rb
+++ lib/low_card_tables/low_card_table/cache.rb
@@ -1,17 +1,43 @@
 module LowCardTables
   module LowCardTable
+    # This class is responsible for caching all the rows for a given low-card table, and then returning various subsets
+    # of those rows on demand.
+    #
+    # This class is actually pretty simple, and it's largely for one big reason: our cache is very simple -- we cache
+    # the entire contents of the table, in memory, and the only way we update it is to throw out the entire cache and
+    # create a brand-new one. As such, from the inside, this class has no mechanisms whatsoever for updating the cache
+    # (as this object is thrown away and a whole new Cache object created when the cache is invalidated) nor are there
+    # any methods for worrying about what should be in cache, doing a LRU, or anything like that.
     class Cache
+      # Creates a new Cache for the given +model_class+, which must be an ActiveRecord::Base subclass that has declared
+      # +is_low_card_table+.
+      #
+      # +options+ can contain:
+      #
+      # [:max_row_count] By default, the cache will raise a fatal error if trying to cache a low-card table that contains
+      #                  more than DEFAULT_MAX_ROW_COUNT (5,000) rows. This is provided so that we detect early on if you
+      #                  start storing data via the low-card system that is not, in fact, of low cardinality. However,
+      #                  if you really are using a low-card table properly and just happen to have more than 5,000
+      #                  distinct combinations of values, you can increase this limit via this option.
       def initialize(model_class, options = { })
+        unless model_class.respond_to?(:is_low_card_table?) && model_class.is_low_card_table?
+          raise ArgumentError, "You must supply a class that is a model class for a low-card table, not #{model_class.inspect}."
+        end
+
         @model_class = model_class
         @options = options
 
         fill!
       end
 
+      # At what time was this cache loaded? This is used (in conjunction with a cache-expiration policy object) to
+      # determine if the cache is stale or not.
       def loaded_at
         @rows_read_at
       end
 
+      # This behaves identically to #rows_matching, except that, everywhere a low-card model object would be returned,
+      # a simple integer ID is returned instead.
       def ids_matching(hash_or_hashes = nil, &block)
         matching = rows_matching(hash_or_hashes, &block)
 
@@ -28,10 +54,18 @@ module LowCardTables
         out
       end
 
+      # Returns an Array of all models of the low-card table in the cache. The order is undefined.
       def all_rows
         @rows_by_id.values
       end
 
+      # Given a single numeric ID of a low-card row, returns that low-card model object.
+      #
+      # Given an Array of one or more IDs of low-card rows, returns a Hash mapping each of those IDs to the
+      # corresponding low-card model object.
+      #
+      # Raises LowCardTables::Errors::LowCardIdNotFoundError if any of the supplied IDs are not found in the cache.
+      # (It is up to calling code -- in our case, the RowManager -- to flush the cache and try again, if desired.)
       def rows_for_ids(id_or_ids)
         ids = Array(id_or_ids)
 
@@ -58,6 +92,25 @@ module LowCardTables
         end
       end
 
+      # Returns a subset of rows in the cache that match a specified set of conditions. (This can be all rows or no
+      # rows, depending on the conditions, or any set in between.)
+      #
+      # You can specify conditions in the following ways:
+      #
+      # * A single Hash, passed as an argument. The return value will be an Array that contains zero or more instances
+      #   of the low-card model class. Only instances that have columns matching the specified Hash will be returned.
+      # * An array of one or more Hashes, passed as an argument. The return value will be a Hash; as keys, it will have
+      #   exactly the Hashes you passed in. The value for each key will be an array of zero or more instances of the
+      #   low-card model class, each of which matches the corresponding key.
+      # * A block, passed to the method as a normal Ruby block. The return value will be an Array that contains zero
+      #   or more instances of the low-card model class. The block will be invoked with every instance of the low-card
+      #   model class, and only those instances where the block returns a value that evaluates to true will be included.
+      #
+      # In the form where you pass in an array of one or more Hashes, it is possible for the same low-card row to show
+      # up in multiple values in the returned Hash, if it matches more than one of the supplied Hashes.
+      #
+      # Note that all matching is done via LowCardTables::LowCardTable::Base#_low_card_row_matches_any_hash?. See that
+      # method for more documentation -- by overriding it, you can change the behavior of this method.
       def rows_matching(hash_or_hashes = nil, &block)
         hashes = hash_or_hashes || [ ]
         hashes = [ hashes ] unless hashes.kind_of?(Array)
@@ -73,12 +126,11 @@ module LowCardTables
         if hashes.length > 0
           out = { }
 
+          hashes.each { |h| out[h] = [ ] }
+
           @rows_by_id.each do |id,r|
             hashes.each do |h|
-              if r._low_card_row_matches_any_hash?([ h ])
-                out[h] ||= [ ]
-                out[h] << r
-              end
+              out[h] << r if r._low_card_row_matches_any_hash?([ h ])
             end
           end
 
@@ -92,19 +144,30 @@ module LowCardTables
         end
       end
 
+      private
+      # Fills the cache. This can only ever be called once.
       def fill!
+        # Explode if we don't have a unique index on the underlying table -- because then we have no way of
+        # guaranteeing that we won't create duplicate rows. (I am of the opinion that you'll never *truly* be certain
+        # that your data obeys rules unless they're in the database, as constraints; application code can be as
+        # careful as it wants to be, but, somehow, things always slip by unless you define constraints at the
+        # database layer.)
         @model_class.low_card_ensure_has_unique_index!
 
         raise "Cannot fill: we already have values!" if @rows_by_id
 
+        # We grab the time before we have fired off the query; this makes sure we don't create a race condition where
+        # we think the cache is just a tiny bit newer than it actually is.
+        read_rows_time = current_time
+
         # We ask for one more than the number of rows we are willing to accept here; this is so that if we have
         # too many rows, we can detect it, but we still won't do something crazy like try to load one million
         # rows into memory.
-        read_rows_time = current_time
-
         raw_rows = @model_class.order("#{@model_class.primary_key} ASC").limit(max_row_count + 1).to_a
         raise_too_many_rows_error if raw_rows.length > max_row_count
 
+        # Yes, we should never have duplicate IDs here -- it should be a primary key. But if it isn't for some reason,
+        # we want to explode right away, rather than failing in strange, unpredicatable, and awful ways later.
         out = { }
         raw_rows.each do |raw_row|
           id = raw_row.id
@@ -116,6 +179,7 @@ module LowCardTables
         @rows_read_at = read_rows_time
       end
 
+      # This is broken out into a separate method merely for ease of testing.
       def current_time
         Time.now
       end
---
+++ spec/low_card_tables/unit/low_card_table/cache_spec.rb
@@ -0,0 +1,252 @@
+require 'low_card_tables'
+
+describe LowCardTables::LowCardTable::Cache do
+  def klass
+    LowCardTables::LowCardTable::Cache
+  end
+
+  it "should require a low-card table class" do
+    mc = double("model_class")
+
+    lambda { klass.new(mc) }.should raise_error(ArgumentError)
+
+    allow(mc).to receive(:is_low_card_table?).and_return(false)
+
+    lambda { klass.new(mc) }.should raise_error(ArgumentError)
+  end
+
+  context "with an instance" do
+    before :each do
+      @mc = double("model_class")
+      allow(@mc).to receive(:is_low_card_table?).and_return(true)
+      allow(@mc).to receive(:low_card_ensure_has_unique_index!)
+      allow(@mc).to receive(:primary_key).and_return("foobar")
+      allow(@mc).to receive(:table_name).and_return("barbaz")
+
+      im1 = double("im1")
+      expect(@mc).to receive(:order).once.with("foobar ASC").and_return(im1)
+      im2 = double("im2")
+      expect(im1).to receive(:limit).once.with(5001).and_return(im2)
+
+      @row1 = double("row1")
+      @row2 = double("row2")
+      @row3 = double("row3")
+
+      allow(@row1).to receive(:id).and_return(1234)
+      allow(@row2).to receive(:id).and_return(1235)
+      allow(@row3).to receive(:id).and_return(1238)
+
+      expect(im2).to receive(:to_a).once.and_return([ @row1, @row2, @row3 ])
+
+      @rows_read_at_time = double("rows_read_at_time")
+      expect(Time).to receive(:now).once.and_return(@rows_read_at_time)
+
+      @cache = klass.new(@mc)
+    end
+
+    it "should correctly expose the time rows were read" do
+      @cache.loaded_at.should be(@rows_read_at_time)
+    end
+
+    it "should return all the rows" do
+      @cache.all_rows.sort_by(&:object_id).should == [ @row1, @row2, @row3 ].sort_by(&:object_id)
+    end
+
+    describe "#rows_matching" do
+      it "should fail if given no hashes" do
+        lambda { @cache.rows_matching([ ]) }.should raise_error(ArgumentError)
+      end
+
+      it "should fail if given something that isn't a Hash" do
+        lambda { @cache.rows_matching([ 12345 ]) }.should raise_error(ArgumentError)
+      end
+
+      it "should fail if neither a hash nor a block" do
+        lambda { @cache.rows_matching }.should raise_error(ArgumentError)
+      end
+
+      it "should fail if given both a hash and a block" do
+        lambda { @cache.rows_matching({ :foo => :bar }) { |x| true } }.should raise_error(ArgumentError)
+      end
+
+      it "should return a Hash if given an array of Hashes" do
+        h1 = { :a => :b, :c => :d }
+        h2 = { :foo => :bar }
+        h3 = { :bar => :baz }
+
+        expect(@row1).to receive(:_low_card_row_matches_any_hash?).once.with([ h1 ]).and_return(true)
+        expect(@row1).to receive(:_low_card_row_matches_any_hash?).once.with([ h2 ]).and_return(true)
+        expect(@row1).to receive(:_low_card_row_matches_any_hash?).once.with([ h3 ]).and_return(false)
+        expect(@row2).to receive(:_low_card_row_matches_any_hash?).once.with([ h1 ]).and_return(false)
+        expect(@row2).to receive(:_low_card_row_matches_any_hash?).once.with([ h2 ]).and_return(true)
+        expect(@row2).to receive(:_low_card_row_matches_any_hash?).once.with([ h3 ]).and_return(false)
+        expect(@row3).to receive(:_low_card_row_matches_any_hash?).once.with([ h1 ]).and_return(false)
+        expect(@row3).to receive(:_low_card_row_matches_any_hash?).once.with([ h2 ]).and_return(false)
+        expect(@row3).to receive(:_low_card_row_matches_any_hash?).once.with([ h3 ]).and_return(false)
+
+        result = @cache.rows_matching([ h1, h2, h3 ])
+        result.class.should == Hash
+        result.size.should == 3
+
+        result[h1].length.should == 1
+        result[h1][0].should be(@row1)
+
+        result[h2].length.should == 2
+        result[h2].sort_by(&:object_id).should == [ @row1, @row2 ].sort_by(&:object_id)
+
+        result[h3].class.should == Array
+        result[h3].length.should == 0
+      end
+
+      it "should return a Hash if given an array of just one Hash" do
+        h1 = { :a => :b, :c => :d }
+
+        expect(@row1).to receive(:_low_card_row_matches_any_hash?).once.with([ h1 ]).and_return(true)
+        expect(@row2).to receive(:_low_card_row_matches_any_hash?).once.with([ h1 ]).and_return(false)
+        expect(@row3).to receive(:_low_card_row_matches_any_hash?).once.with([ h1 ]).and_return(false)
+
+        result = @cache.rows_matching([ h1 ])
+        result.class.should == Hash
+        result.size.should == 1
+
+        result[h1].length.should == 1
+        result[h1][0].should be(@row1)
+      end
+
+      it "should return an Array if given a Hash" do
+        h1 = { :a => :b, :c => :d }
+
+        expect(@row1).to receive(:_low_card_row_matches_any_hash?).once.with([ h1 ]).and_return(true)
+        expect(@row2).to receive(:_low_card_row_matches_any_hash?).once.with([ h1 ]).and_return(false)
+        expect(@row3).to receive(:_low_card_row_matches_any_hash?).once.with([ h1 ]).and_return(true)
+
+        result = @cache.rows_matching(h1)
+        result.class.should == Array
+        result.sort_by(&:object_id).should == [ @row1, @row3 ].sort_by(&:object_id)
+      end
+
+      it "should return an Array if given a Block" do
+        block = lambda { }
+
+        expect(@row1).to receive(:_low_card_row_matches_block?).once.with(block).and_return(true)
+        expect(@row2).to receive(:_low_card_row_matches_block?).once.with(block).and_return(true)
+        expect(@row3).to receive(:_low_card_row_matches_block?).once.with(block).and_return(false)
+
+        result = @cache.rows_matching(&block)
+        result.class.should == Array
+        result.sort_by(&:object_id).should == [ @row1, @row2 ].sort_by(&:object_id)
+      end
+    end
+
+    describe "#ids_matching" do
+      it "should fail if given no hashes" do
+        lambda { @cache.ids_matching([ ]) }.should raise_error(ArgumentError)
+      end
+
+      it "should fail if given something that isn't a Hash" do
+        lambda { @cache.ids_matching([ 12345 ]) }.should raise_error(ArgumentError)
+      end
+
+      it "should fail if neither a hash nor a block" do
+        lambda { @cache.ids_matching }.should raise_error(ArgumentError)
+      end
+
+      it "should fail if given both a hash and a block" do
+        lambda { @cache.ids_matching({ :foo => :bar }) { |x| true } }.should raise_error(ArgumentError)
+      end
+
+      it "should return a Hash if given an array of Hashes" do
+        h1 = { :a => :b, :c => :d }
+        h2 = { :foo => :bar }
+        h3 = { :bar => :baz }
+
+        expect(@row1).to receive(:_low_card_row_matches_any_hash?).once.with([ h1 ]).and_return(true)
+        expect(@row1).to receive(:_low_card_row_matches_any_hash?).once.with([ h2 ]).and_return(true)
+        expect(@row1).to receive(:_low_card_row_matches_any_hash?).once.with([ h3 ]).and_return(false)
+        expect(@row2).to receive(:_low_card_row_matches_any_hash?).once.with([ h1 ]).and_return(false)
+        expect(@row2).to receive(:_low_card_row_matches_any_hash?).once.with([ h2 ]).and_return(true)
+        expect(@row2).to receive(:_low_card_row_matches_any_hash?).once.with([ h3 ]).and_return(false)
+        expect(@row3).to receive(:_low_card_row_matches_any_hash?).once.with([ h1 ]).and_return(false)
+        expect(@row3).to receive(:_low_card_row_matches_any_hash?).once.with([ h2 ]).and_return(false)
+        expect(@row3).to receive(:_low_card_row_matches_any_hash?).once.with([ h3 ]).and_return(false)
+
+        result = @cache.ids_matching([ h1, h2, h3 ])
+        result.class.should == Hash
+        result.size.should == 3
+
+        result[h1].length.should == 1
+        result[h1][0].should == @row1.id
+
+        result[h2].length.should == 2
+        result[h2].sort.should == [ @row1.id, @row2.id ].sort
+
+        result[h3].class.should == Array
+        result[h3].length.should == 0
+      end
+
+      it "should return a Hash if given an array of just one Hash" do
+        h1 = { :a => :b, :c => :d }
+
+        expect(@row1).to receive(:_low_card_row_matches_any_hash?).once.with([ h1 ]).and_return(true)
+        expect(@row2).to receive(:_low_card_row_matches_any_hash?).once.with([ h1 ]).and_return(false)
+        expect(@row3).to receive(:_low_card_row_matches_any_hash?).once.with([ h1 ]).and_return(false)
+
+        result = @cache.ids_matching([ h1 ])
+        result.class.should == Hash
+        result.size.should == 1
+
+        result[h1].length.should == 1
+        result[h1][0].should == @row1.id
+      end
+
+      it "should return an Array if given a Hash" do
+        h1 = { :a => :b, :c => :d }
+
+        expect(@row1).to receive(:_low_card_row_matches_any_hash?).once.with([ h1 ]).and_return(true)
+        expect(@row2).to receive(:_low_card_row_matches_any_hash?).once.with([ h1 ]).and_return(false)
+        expect(@row3).to receive(:_low_card_row_matches_any_hash?).once.with([ h1 ]).and_return(true)
+
+        result = @cache.ids_matching(h1)
+        result.class.should == Array
+        result.sort.should == [ @row1.id, @row3.id ].sort
+      end
+
+      it "should return an Array if given a Block" do
+        block = lambda { }
+
+        expect(@row1).to receive(:_low_card_row_matches_block?).once.with(block).and_return(true)
+        expect(@row2).to receive(:_low_card_row_matches_block?).once.with(block).and_return(true)
+        expect(@row3).to receive(:_low_card_row_matches_block?).once.with(block).and_return(false)
+
+        result = @cache.ids_matching(&block)
+        result.class.should == Array
+        result.sort.should == [ @row1.id, @row2.id ].sort
+      end
+    end
+
+    describe "#rows_for_ids" do
+      it "should return a single row for a single ID" do
+        @cache.rows_for_ids(@row2.id).should be(@row2)
+      end
+
+      it "should return a map for multiple IDs" do
+        result = @cache.rows_for_ids([ @row1.id, @row2.id ])
+        result.class.should == Hash
+        result.size.should == 2
+        result[@row1.id].should be(@row1)
+        result[@row2.id].should be(@row2)
+      end
+
+      it "should return a map for a single ID, passed in an array" do
+        result = @cache.rows_for_ids([ @row3.id ])
+        result.class.should == Hash
+        result.size.should == 1
+        result[@row3.id].should be(@row3)
+      end
+
+      it "should blow up if passed a missing ID" do
+        lambda { @cache.rows_for_ids([ @row1.id, 98765, @row3.id ]) }.should raise_error(LowCardTables::Errors::LowCardIdNotFoundError, /98765/)
+      end
+    end
+  end
+end
